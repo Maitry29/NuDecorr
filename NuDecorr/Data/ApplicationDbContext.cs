76e771d60063a5e34f7360045f31062c1d66f35f
Maitry <maitrithaker1239@gmail.com>
2024-12-26 16:54:50 +0530

    save

---
+++ NuDecorr/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NuDecorr.Models;
+
+namespace NuDecorr.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+               new Category { Id = 1, Name = "Table", DisplayOrder = 1 },
+               new Category { Id = 2, Name = "Chair", DisplayOrder = 2 },
+               new Category { Id = 3, Name = "Sofa", DisplayOrder = 1 },
+               new Category { Id = 4, Name = "Lamp", DisplayOrder = 3 },
+               new Category { Id = 5, Name = "Bed", DisplayOrder = 1 }
+            );
+        }
+    }
+}
---
+++ NuDecorr/Migrations/20241224133928_SeedCategoryTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NuDecorr.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Table" },
+                    { 2, 2, "Chair" },
+                    { 3, 1, "Sofa" },
+                    { 4, 3, "Lamp" },
+                    { 5, 1, "Bed" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
